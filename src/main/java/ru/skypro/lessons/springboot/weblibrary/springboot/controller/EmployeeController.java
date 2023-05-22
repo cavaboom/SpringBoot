7f4508d78908595f3e6f268a32191e436d3258b6
dendariev <den.dariev@gmail.com>
2023-05-22 21:56:17 +0800

    Добавлены пути

--- src/main/java/ru/skypro/lessons/springboot/weblibrary/springboot/controller/EmployeeController.java
+++ src/main/java/ru/skypro/lessons/springboot/weblibrary/springboot/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package ru.skypro.lessons.springboot.weblibrary.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,26 +12,27 @@ public class EmployeeController {
 
     private final EmployeeService employeeService;
 
+    @Autowired
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
 
-    @GetMapping
+    @GetMapping("/salarySum")
     public String getEmployeeSalarySum() {
         return employeeService.getEmployeeSalarySum();
     }
 
-    @GetMapping
+    @GetMapping("/salaryMin")
     public String getEmployeeSalaryMin() {
         return employeeService.getEmployeeSalaryMin();
     }
 
-    @GetMapping
+    @GetMapping("/salaryMax")
     public String getEmployeeSalaryMax() {
         return employeeService.getEmployeeSalaryMax();
     }
 
-    @GetMapping
+    @GetMapping("/highSalary")
     public String getEmployeeAverageHighSalary() {
         return employeeService.getEmployeeAverageHighSalary();
     }
